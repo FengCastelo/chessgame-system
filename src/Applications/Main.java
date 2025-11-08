e013b70f27580dc88a64e0648523b31cd4281384
FengCastelo <ianfcastelo7@gmail.com>
2025-11-08 02:13:03 -0300

    Position Class

---
+++ src/Applications/Main.java
@@ -0,0 +1,11 @@
+package Applications;
+
+import BoardGame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position ps1 = new Position(3, 5);
+
+        System.out.println(ps1);
+    }
+}
